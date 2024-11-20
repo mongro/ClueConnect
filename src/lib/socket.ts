fd080993d72721e224f1904b066210904711cbf5
mongro <simon.gross04@web.de>
2024-11-20 01:15:35 +0100

    -disable autoconnect socket

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,8 @@
 import type { ClientToServerEvents, ServerToClientEvents } from '$shared/src/types';
+import { PUBLIC_SERVER_URL } from '$env/static/public';
 import { io, Socket } from 'socket.io-client';
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:5000');
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(PUBLIC_SERVER_URL, {
+	autoConnect: false
+});
 console.log('socket');
 export default socket;
